6c57056fe9d062ac1dd3c513b341cabe3225680e
Agustin Peluffo <agustin.peluffo@fing.edu.uy>
2017-06-06 16:17:45 -0300

    Generator.hs: Anda para todo menos let

--- tarea/Generator.hs
+++ tarea/Generator.hs
@@ -12,22 +12,51 @@ import Syntax
 -- se pueden agregar mas importaciones 
 -- en caso de ser necesario
 
+instance Show Op where
+    show Add = "+"
+    show Sub = "-"
+    show Mult = "*"
+    show Div = "div"
+    show Eq = "="
+    show NEq = "/="
+    show GTh = ">"
+    show LTh = "<"
+    show GEq = ">="
+    show LEq = "<="
 
 -- CODE GENERATOR
 
 
 genProgram :: Program -> String
-genProgram _ = "¡Generador en obras!"
 
---genProgram (Program dfs expr)  = "#include <stdio.h>\n" ++ (genDfs dfs) ++ "\nint main(){" ++ (genExpr expr) ++ "}" -- Implementar
 
-{--
-genDfs :: Defs -> String
-genDfs _ = ""
+genProgram (Program dfs expr)  = "#include <stdio.h>\n" ++ (generateDefs dfs) ++ "int main() {\nprintf(\"%d\\n\"," ++ (genExpr expr) ++ "); }" -- Implementar
+
+
+generateDefs :: Defs -> String
+generateDefs [] = []
+generateDefs (x:[]) = (generateDefinition x) ++ "\n"
+generateDefs (x:xs) = (generateDefinition x) ++ (generateDefs xs)
+
+
+generateDefinition :: FunDef -> String
+generateDefinition (FunDef (n,s) nx expr) = "int _" ++ show n ++ "(" ++ name2param nx ++ "){\nreturn (" ++ genExpr expr ++ "); };"
+
+name2param :: [Name] -> String
+name2param [] = []
+name2param (x:[]) = "int _" ++ (show x)
+name2param (x:xs) = "int _" ++ (show x) ++ "," ++ name2param xs
+
+expr2param :: [Expr] -> String
+expr2param [] = []
+expr2param (x:[]) = (genExpr x)
+expr2param (x:xs) = (genExpr x) ++ "," ++ (expr2param xs)
 
 genExpr :: Expr -> String
-genExpr (Var x) = show x ++ " "
-genExpr (IntLit x) =  show x ++ " "
-genExpr (BoolLit x) = show x ++ " "
-genExpr (Infix op x y) = (genExpr x) ++ " " ++ show op ++ " " ++ (genExpr y)
---}
+genExpr (Var x) = "_" ++ show x
+genExpr (IntLit x) =  show x 
+genExpr (BoolLit x) = if x then "1" else "0" 
+genExpr (Infix op x y) = "(" ++ (genExpr x) ++ " " ++ show op ++ " " ++ (genExpr y) ++ ")"
+genExpr (If e1 e2 e3) = (genExpr e1) ++ "?" ++ (genExpr e2) ++ ":" ++ (genExpr e3)
+genExpr (App n ns) = "_" ++ show n ++ "(" ++ (expr2param ns) ++ ")"
+genExpr (Let _ _ _) = "Acá va un let!"
